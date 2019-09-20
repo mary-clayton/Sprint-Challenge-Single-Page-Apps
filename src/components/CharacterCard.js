16286a98f269586f38d23a1038607dfabd8e04de
Mary <mary.clayton1@hotmail.com>
2019-09-20 11:28:00 -0700

    met mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,23 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+function CharacterCard(props) {
+  // return <span>todo: character</span>;
+  console.log(props);
+  return (
+    <div className="charactercard">
+      <NavLink className= "links" exact to={`/characters/${props.id}`}>
+        <div className="card">
+      <img src={props.image} alt="character"/>
+      <h1>Name: {props.name}</h1>
+      <p>Gender: {props.gender}</p>
+      <p>Status: {props.status}</p>
+      <p>species: {props.species}</p>
+      </div>
+      </NavLink>
+    </div>
+  )
 }
+
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState
+} from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import {Row, Container} from 'reactstrap'
 
-export default function CharacterList() {
+const CharacterList = () => {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter] = useState([]);
   useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        // console.log(res.data);
+        const characters = res.data;
+        setCharacter(characters);
+      })
+      .catch(err => {
+        console.log("It's just an error lol", err)
+      })
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
+  if (!character.results) {
+    return (
+      <div>
+        <h2>Fetching required data</h2>
+        <p>This should only take a moment to fetch the data.</p>
+      </div>
+    );
+  }
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Characters</h2>
+      <Container>
+        <Row className="rows"> 
+          {character.results.map((character, index) => {
+            return (<CharacterCard key={index} {...character} />)
+          })}
+        </Row>
+      </Container>
     </section>
   );
 }
+  
+export default CharacterList;
